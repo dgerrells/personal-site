45409b34d41c6c5b589e121d9677e2a2aa6c7bc2
David Gerrells <David Gerrells>
2016-09-21 13:42:12 -0600

    Initial commit

--- index/main.js
+++ index/main.js
@@ -1,15 +1,16 @@
 (function () {
     var getClickHandler = function ($element, dest, replace) {
         return function () {
+            $('.backdrop').attr('style', 'opacity: 1;')
+            $element.attr("style", "z-index: 35;");
             $element.velocity({
                 properties: {
                     scale: 1.8,
                     opacity: 0
                 },
                 options: {
-                    duration: 800,
+                    duration: 700,
                     easing: "easeOutQuart",
-                    mobileHA: false,
                     complete: function () {
                         $element.attr("style", "");
                         if (replace) {
@@ -33,7 +34,7 @@
     $blog.click(getClickHandler($blog, 'https://www.tekker.solutions', true));
     $projects.click(getClickHandler($projects, './projects/projects.html', true));
     $email.click(getClickHandler($email, 'mailto:dgerrell@msudenver.edu'));
-    $linkedin.click(getClickHandler($linkedin, 'https://www.linkedin.com/in/david-gerrells-73a9108a'));
-    $github.click(getClickHandler($github, 'https://github.com/dgerrells'));
-    $codepen.click(getClickHandler($codepen, 'https://codepen.io/gerrells-david/'));
+    $linkedin.click(getClickHandler($linkedin, 'https://www.linkedin.com/in/david-gerrells-73a9108a', true));
+    $github.click(getClickHandler($github, 'https://github.com/dgerrells', true));
+    $codepen.click(getClickHandler($codepen, 'https://codepen.io/gerrells-david/', true));
 })();
