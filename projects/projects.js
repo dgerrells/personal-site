0eba837ef9747ff14f51a9ca138a82d4e0adcf32
David Gerrells <David Gerrells>
2016-12-08 15:16:08 -0700

    add project

--- projects/projects.js
+++ projects/projects.js
@@ -1,10 +1,16 @@
 (function () {
+
     $('.card-wrapper').bind({
         mouseenter: function() {
+            $('.card').removeClass('flipped');
             $(this).find('.card').addClass('flipped');
         },
         mouseleave: function() {
             $(this).find('.card').removeClass('flipped');
+        },
+        mousedown: function() {
+            $('.card').removeClass('flipped');
+            $(this).find('.card').addClass('flipped');
         }
     })
 })();
